74e47ab785dea8dbd4c9fe9dfb99aec17372e717
RafaIzu <bomizukawa@gmail.com>
2020-05-23 19:18:49 -0300

    adicionando a pagina de recado(sem o arquivo.bd)

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,53 @@
 from flask import Flask, render_template 
 app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mensagem.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Mensagem(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nome = db.Column(db.String(25), nullable=True)
+    sobrenome = db.Column(db.String(25), nullable=True)
+    email = db.Column(db.String(25), nullable=True)
+    assunto =  db.Column(db.String(50), nullable=True )
+    mensagem = db.Column(db.String(100), nullable=True)
+    hora = db.Column(db.DateTime, default=datetime.now)
+
+
+@app.route('/comunicado', methods = ['POST'])
+def visitante():
+    if request.method == 'POST':
+        print(request.form['nome'])
+        print(request.form['sobrenome'])
+        print(request.form['email'])
+        print(request.form['assunto'])
+        print(request.form['mensagem'])
+        visita = Mensagem(nome = request.form['nome'], sobrenome=request.form['sobrenome'],email=request.form['email'],assunto = request.form['assunto'],mensagem=request.form['mensagem'])
+        db.session.add(visita)
+        db.session.commit()
+        return render_template('/obrigado.html')
+
 
 @app.route('/inicio')
 @app.route('/')
 def inicio():
     return render_template('index.html')
+
+
+@app.route('/contato')
+def contato():
+    return render_template('/contato.html')
+
+
+@app.route('/obrigado')
+def obrigado():
+    return render_template('/obrigado.html')
+
 
 @app.route('/login')
 def pagina_curriculo():
