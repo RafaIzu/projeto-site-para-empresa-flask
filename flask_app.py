443129160de9f39c48ce127c14c641211d9d76cd
RafaIzu <bomizukawa@gmail.com>
2020-05-24 17:10:15 -0300

    adicionando banco de dados funcionarios

--- flask_app.py
+++ flask_app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from pytz import timezone
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mensagem.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bancoDedados.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -17,6 +18,36 @@ class Mensagem(db.Model):
     assunto =  db.Column(db.String(50), nullable=True )
     mensagem = db.Column(db.String(100), nullable=True)
     hora = db.Column(db.DateTime, default=datetime.now)
+
+
+class Apontamento(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    nome_usuario = db.Column(db.String(25), nullable = True)
+    ##hora = db.Column(db.DateTime, default = datetime.now)
+    hora = db.Column(db.DateTime, default = datetime.now().astimezone(timezone('America/Sao_Paulo')))
+
+    @property
+    def serializar(self):
+        return {
+            'id' : self.id,
+            'nome_usuario' : self.nome_usuario,
+            'hora' : str(self.hora)[11:16]
+        }
+
+
+@app.route('/funcionarios', methods = ['GET', 'POST'])
+def funcionarios():
+    if request.method == 'POST':
+        print(request.form['nome']) 
+        usuario = Apontamento(nome_usuario = request.form['nome'])
+        db.session.add(usuario)
+        db.session.commit()
+        return render_template('index.html')
+
+    if request.method == 'GET':
+        ultimos10 = Apontamento.query.order_by(-Apontamento.id).limit(10).all()
+        print(ultimos10)
+        return jsonify(funcionarios= [i.serializar for i in ultimos10])
 
 
 @app.route('/comunicado', methods = ['POST'])
